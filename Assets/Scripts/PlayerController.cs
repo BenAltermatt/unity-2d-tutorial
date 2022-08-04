88118be16e2a995cbe422eec0b4966f933c7c765
BenAltermatt <ben.altermatt20@gmail.com>
2022-08-04 13:44:43 -0400

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Attributes
+    [Header("Speed")]
+    [SerializeField] private float speed;
+
+    [Header("Moving")]
+    [SerializeField] private bool moving;
+    [Header("Direction")]
+    [SerializeField] private uint direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
